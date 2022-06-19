f4f5e5c3e373864c8bc1bd90cf98bdfdd8f2da8c
IDAGIZA <ruthidagiza@gmail.com>
2022-06-19 23:59:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({name,email})
+    })
+      .then(function (res){
+        return res.json()
+      })
+      .then(function (dataRecieved){
+        document.body.innerHTML= dataRecieved["id"]
+      })
+      .catch(function (error) {
+        document.body.innerHTML = error.message
+      });
+
+  }
+  console.log(submitData(ruth, idagiza))
